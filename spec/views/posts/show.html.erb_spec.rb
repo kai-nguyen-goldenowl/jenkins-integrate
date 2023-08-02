8711a9d1cff7d12d79a8d6a91c300e4c78903f21
Kai <kai.nguyen.goldenowl@gmail.com>
2023-08-02 09:50:41 +0700

    Init Project

---
+++ spec/views/posts/show.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "posts/show", type: :view do
+  before(:each) do
+    assign(:post, Post.create!(
+      content: "Content",
+      title: "Title"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Content/)
+    expect(rendered).to match(/Title/)
+  end
+end
